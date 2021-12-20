21e893270d8cc376807cd4f0d66b5db1131ac4c8
yul <1103221645@qq.com>
2021-12-20 16:51:24 +0800

    新增页面水印

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="about">
-    <el-button type="primary">123</el-button>
-    <el-color-picker v-model="color" @change="changeColor"></el-color-picker>
     <div class="div">全局</div>
     是否灰色<el-switch
       v-model="value3"
@@ -10,21 +8,26 @@
       inactive-text="否"
     >
     </el-switch>
+    <el-divider></el-divider>
+    开启水印<el-switch
+      v-model="wart"
+      @change="onSwitchChange"
+      active-text="是"
+      inactive-text="否"
+    ></el-switch>
   </div>
 </template>
 
 <script setup>
   import { reactive, ref, toRefs } from '@vue/reactivity'
   import { watch } from '@vue/runtime-core'
-
-  const color = ref('')
+  import wartermark from '@utils/wartermark.ts'
 
   const value3 = ref(false)
+  const wart = ref(false)
 
-  const changeColor = (val) => {
-    // 改变主题色
-    const el = document.getElementsByTagName('body')[0]
-    el.style.setProperty('--el-button-bg-primary', val)
+  const onSwitchChange = (value) => {
+    value ? wartermark.set('vue3-admin') : wartermark.del()
   }
 
   watch(
